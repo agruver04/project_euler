254710e72c39b106935a4fb9d0f1acc24ad345dd
agruver04 <agruver@g.hmc.edu>
2022-11-02 22:35:07 -0400

    working (extremely inefficient) answer for 77

--- 77/77.py
+++ 77/77.py
@@ -1,4 +1,7 @@
 from typing import Dict
+from dataclasses import dataclass
+from typing import List
+import math
 
 # Generate a dictionary with info of all the primes up to 1000000
 primes = {}
@@ -12,8 +15,7 @@ for i in range (2, 1000000):
         for j in range(i * 2, 1000000, i):
             primes[j] = False
 
-prime_summations = {2:1, 3:1}
-
+'''
 def recursive_permutations(i: int) -> int:
 
     lower_primes = [j for j in prime_summations.keys() if j < i]
@@ -25,18 +27,70 @@ def recursive_permutations(i: int) -> int:
         sum += recursive_permutations(i - prime)
 
     return sum
+'''
+@dataclass
+class PrimeChain:
+    primes_so_far: List[str]
+    remainder: int
 
-latest = 4
+latest = 65
 
-while latest < 20:
-    print(latest)
+while True:
 
-    prime_summations[latest] = recursive_permutations(latest)
+    chains = [PrimeChain(primes_so_far=[], remainder=latest)]
+    solutions = []
 
-    if prime_summations[latest] > 5000:
-        print(latest)
+    # For each prime smaller than the latest number, we add it to all
+    # of the existing chains of primes
+    smaller_primes = [p for p in primes.keys() if p <= latest and primes[p]]
+
+    print(smaller_primes)
+
+    # Continue to iterate until the list of active chains is empty
+    while chains:
+
+        new_chains = []
+
+        for chain in chains:
+
+            # If we see a chain who's remainder is 0, break but keep that
+            # chain, we've found a solution
+            if chain.remainder == 0:
+                solutions.append(chain)
+                continue
+
+            # If we see a chain who's remainder is 1, break and don't keep it
+            if chain.remainder == 1:
+                continue
+
+            # Otherwise, we still have work to do on that chain
+            for prime in smaller_primes:
+
+                # If the prime is smaller than the remainder, we can add it
+                if prime <= chain.remainder:
+
+                    # Create a new sorted chain with the prime added
+                    new_sorted_chain = sorted(chain.primes_so_far + [prime])
+                    new_chain = PrimeChain(primes_so_far=new_sorted_chain,
+                                           remainder=chain.remainder - prime)
+                    new_chains.append(new_chain)
+
+        chains = []
+
+        # And remove any duplicates
+        for chain in new_chains:
+            if chains.count(chain) == 0:
+                chains.append(chain)
+
+        pass
+
+    print(f'Number: {latest}, Solutions: {len(solutions)}')
+
+    if len(solutions) > 5000:
+        print(f'Number: {latest}, Solutions: {len(solutions)}')
         break
 
-    latest = latest + 1
+    # for solution in solutions:
+    #     print(f'{solution.primes_so_far}')
 
-print(prime_summations)
+    latest = latest + 1
